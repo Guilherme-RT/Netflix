84cedcb419c808c1600ad8ea87c9023b7ae3de3b
Guilherme-RT <guilhermeradztoledo92@hotmail.com>
2020-11-06 12:51:17 -0300

    Netflix Dual commitado

---
+++ Netflix Dual/js/owlc/config.js
@@ -0,0 +1,17 @@
+// Comandos de Carrossel Autoplay
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 5,
+    loop: true,
+    margin: 15,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplayHoverPause: true
+});
+$('.play').on('click', function() {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function() {
+    owl.trigger('stop.owl.autoplay')
+})
